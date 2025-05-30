a465913e9fe64795722ee78f8d8a0a10d09889a6
FabioFernandesDev <fabiofernandesrt@gmail.com>
2025-05-30 11:04:09 -0300

    Criação do manu

---
+++ PythonProject/cadastramentodeusuarios.py
@@ -0,0 +1,12 @@
+#Cadastramento de Usuarios
+usuario= []
+
+def menu(): #Menu de acesso as opções 
+ print("MENU DE ACESSO")
+ print("Cadastar usuarios")
+ print("Listar Usuarios")
+ print("Buscar Usuario por nome")
+ print("Remover Usuario")
+ print("Sair")
+ opçao= input("Escolha uma Opção")
+ return opçao
