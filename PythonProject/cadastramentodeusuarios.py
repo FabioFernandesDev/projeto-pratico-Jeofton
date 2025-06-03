ffd8d75da87fbd3cf34d59915b4c1655158e21d7
FabioFernandesDev <fabiofernandesrt@gmail.com>
2025-06-03 12:10:58 -0300

    updates e correções de erros

--- PythonProject/cadastramentodeusuarios.py
+++ PythonProject/cadastramentodeusuarios.py
@@ -2,38 +2,39 @@
 usuario= []
 
 def menu(): #Menu de acesso as opções 
- print("MENU DE ACESSO")
- print("Cadastar usuarios")
- print("Listar Usuarios")
- print("Buscar Usuario por nome")
- print("Remover Usuario")
- print("Sair")
+ print("\nMENU DE ACESSO")
+ print("1. Cadastar usuarios")
+ print("2. Listar Usuarios")
+ print("3. Buscar Usuario por nome")
+ print("4. Remover Usuario")
+ print("5. Sair")
  opcao= input("Escolha uma Opção")
  return opcao
 
 def adicionar_usuario(): #Opção de Adicionar Usuario
+ print("n--- Cadastro de Novo Usuario ---")
  nome = input("Digite o nome do usuario")
  idade = input("Digite a idade do usuario")
  email = input("digite o email do usuario")
  contato = input("digite o telefone do usuario")
 
-usuario = {
- "nome": nome,
- "idade": idade,
- "email": email,
- "contato": contato
+novo_usuario = {
+    "nome": nome,
+    "idade": idade,
+    "email": email,
+    "contato": contato
 }
 
-usuario.append(usuario)
-print("Usuario adicionado com sucesso")
+usuarios.append(novo_usuario)
+print("\nUsuario adicionado com sucesso")
 
-def listar_usuario(): #opção para listar usuario
+def listar_usuarios(): #opção para listar usuario
  if not usuarios:
-  print("Nenhum usuario cadastrado.")
-  return
+    print("\nNenhum usuario cadastrado.")
+    return
  
- print("LISTA DE USUARIOS")
- for i, usuario in enumerate(usuarios,start=1):
+ print("\n--- LISTA DE USUARIOS ---")
+ for i, usuario in enumerate(usuarios, start=1):
   print(f"{i}. Nome:{usuario['nome']}, Idade: {usuario['idade']}, Email:{usuario['email']}, Contato: {usuario['contato']}")
 
   
